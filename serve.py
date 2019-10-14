49e31b64535aeacff6dd3dbbed375b981a77bd5e
wianoski <thirafi.wian@gmail.com>
2019-10-14 14:04:32 +0700

    insert

--- serve.py
+++ serve.py
@@ -1,7 +1,33 @@
 import socket
 import json
+import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
 
-print("Waiting connection....")
+
+
+def insertData(data):
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                            database='fingerLog',
+                                            user='wianoski',
+                                            password='')
+
+        queryInsert = """INSERT INTO log_finger (finger_id, finger_number, finger_time) VALUES (NULL, """+ data +""", NULL)"""
+
+
+        cursor = connection.cursor()
+        result = cursor.execute(queryInsert)
+        connection.commit()
+        print("Record inserted successfully into db")
+        # cursor.close()
+
+    except mysql.connector.Error as error:
+        print("Failed to insert record into  {}".format(error))
+
+
+
+print("Waiting for all connection....")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(('localhost', 3040))
     s.listen()
@@ -15,9 +41,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if not data:
                 break
             # conn.sendall(data)
+
+
             this = data
             # this = data
             print('you got data: ',this.decode("utf-8"))
+            insertData(this.decode("utf-8"))
 
             f = open("data.txt", 'w')
             f.write(this.decode("utf-8"))
